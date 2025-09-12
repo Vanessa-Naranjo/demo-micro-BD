26a5b0f8b2f7e831bc9c76881fbd78f0d231021a
Vanessa Naranjo <vanelau_94@hotmail.com>
2025-09-12 10:02:28 -0500

    Proyecto micro BD

---
+++ src/main/java/com/lvnr/demo/micro/bd/DemoMicroBdApplication.java
@@ -0,0 +1,13 @@
+package com.lvnr.demo.micro.bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicroBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicroBdApplication.class, args);
+	}
+
+}
